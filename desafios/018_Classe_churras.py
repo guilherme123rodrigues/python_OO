5a5f80100df32ca55ecdbb19dbb5958c766adf9d
Guilherme Rodrigues <guigithub001@gmail.com>
2026-04-14 20:58:55 -0300

    Update 018_Classe_churras.py

--- desafios/018_Classe_churras.py
+++ desafios/018_Classe_churras.py
@@ -8,37 +8,43 @@ console = Console()
 class Churras():
     '''
     
-    Essa classe serve para calcular o total de carne que deverá commpra, e calcular o total a ser gasto por peesso
+    Essa classe serve para calcular o total de carne que deverá compra, e calcula o total a ser gasto por pessoa.
 
     Sintax:
 
-    Variavel = Churras(quantidade = valor obrigatiorio)
+    Variavel = Churras(titulo, quantidade de pessoa)
     
     '''
-    def __init__(self, quantidade):
-        #Atirbutos da classe
+    def __init__(self, titulo, quantidade):
+        #Atirbutos da instância
+        self.titulo = titulo
         self.quantidade = quantidade
 
     #Metodo da classe
     def calculo(self):
-        kg = 500
+        kg = 400
         preço = 82.40
         t_carne = self.quantidade * kg
 
-        #Fiz um condição, se a quantidade de pessoa for == 1 pego o resto de t_carne % 1000 que vai ser 500
-        #senão, faço a divissão normal.
-        k_p = str(t_carne % 1000) + 'g' if self.quantidade == 1 else str(t_carne / 1000) + ' kg'
+        #Fiz uma condição, se a quantidade de pessoa for == 1 pego o resto de t_carne % 1000 que vai ser 500
+        #Senão, faço a divissão normal.
+        k_p = str(t_carne % 1000) + 'g' if 1 <= self.quantidade <= 2 else str(t_carne / 1000) + ' kg'
         tot_pagar = (t_carne * preço) / 1000
 
         console.print(Panel(f'Quantidade de pessoa:\t{self.quantidade}\n'
-                            f'Kilo de carne:\t\t{k_p}\n' 
-                            f'Total a ser pago:\tR${tot_pagar:.2f}'
-                            , title='[bold italic yellow]Churras[/]'
+                            f'Quantidade de carne:\t{k_p}\n' 
+                            f'Total a ser pago:\tR${tot_pagar:.2f}\n'
+                            f'Cada um vai pagar: \tR${tot_pagar/self.quantidade:.2f}'
+                            , title=f'[bold italic yellow]{self.titulo}[/]'
                             , width=50
                             )
                     )
 
 
 #Criação do objeto
-p1 = Churras(0)
+p1 = Churras('Churras dos amigos', 15)
+p1.calculo()
+
+#Criação do objeto
+p1 = Churras('Festa fim de ano', 35)
 p1.calculo()
