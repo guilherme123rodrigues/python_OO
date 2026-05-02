ff991cdc3692f376b1a1ec3e2f53c4bec2f824c3
Guilherme Rodrigues <guigithub001@gmail.com>
2026-05-01 22:28:42 -0300

    Update 019_Classe_Livro.py

--- desafios/019_Classe_Livro.py
+++ desafios/019_Classe_Livro.py
@@ -9,10 +9,29 @@ class Livro():
     def __init__(self, autor, paginas):
         self.autor = autor
         self.paginas = paginas
+        self.go = 1
 
     #Metodos da classe
-    def passar_pagina(self):
-        t = False
+    def passar_pagina(self, passa):
+        console.print(f'Você acabou de abrir o livro [bold yellow italic]{self.autor}[/] que tem [bold green italic]{self.paginas}[/] página(s) no total. Você agora está na página {self.go}')
+        cont = 0
+        
+        for c in range(self.go+1, (passa+self.go+1)):
+            if c <= self.paginas:
+                if c < (passa+self.go) and c < self.paginas:
+                    console.print(f'[blue italic]Pág.{c}[/] =>',end=' ')
+                else:
+                    console.print(f'[blue italic]Pág.{c}[/]. Você acabou de avançar {cont+1} página(s)')
+            else:
+                console.print(f'\n❌[red italic]Chegamos ao fim do seu livro {self.autor}[/]❌')
+                return
+            cont += 1
+        self.go += cont
+        console.print()
+
+
+
+        '''t = False
         p2 = 0 
         p3 = 0
         while True:
@@ -45,8 +64,10 @@ class Livro():
             if t == True:
                 break
             print('\n')
-
+'''
 
 #Criação do objeto da classe Livro
 p1 = Livro('Thiago Nitro', 20)
-p1.passar_pagina()
+
+p1.passar_pagina(5)
+p1.passar_pagina(10)
